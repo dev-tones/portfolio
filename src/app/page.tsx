996caed744f9cb6bdb2bbf0994fd561aca913c9e
Matthew Twigg <dev_tones@outlook.com>
2025-08-16 23:32:00 -0400

    Home page alteration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,12 @@
 export default function Home() {
   return (
     <>
-      
+      <h1 className="text-3xl">
+        About
+      </h1>
+      <p>
+        I am a passionate developer enthusiastic about learning, solving real problems, and beautiful design.
+      </p>
     
     </>
   );

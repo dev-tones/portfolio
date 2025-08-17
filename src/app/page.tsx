5752fbbfae58058d484b2bc2cb5d34db77fbf91d
Matthew Twigg <dev_tones@outlook.com>
2025-08-17 10:03:01 -0400

    page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@
 export default function Home() {
   return (
     <>
-      <span className="w-max">
-        <h1 className="text-3xl justify-center-safe">
+      <div className="w-2/3 grid grid-rows-2 grid-cols-3">
+        <h1 className="col-span-1 col-start-2 text-4xl text-center font-mono antialiased shrink">
           About
         </h1>
-        <p>
+        <p className="font-mono antialiased row-span-1 row-start-2 col-start-2 shrink">
           I am a passionate developer enthusiastic about learning, solving real problems, and beautiful design.
         </p>
-      </span>
+      </div>
     </>
   );
 }

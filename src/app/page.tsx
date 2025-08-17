264f35507c5caaa8c44cc0916901b30720e83ef0
Matthew Twigg <dev_tones@outlook.com>
2025-08-17 10:19:35 -0400

    flexed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@
 export default function Home() {
   return (
     <>
-      <div className="w-2/3 grid grid-rows-2 grid-cols-3">
-        <h1 className="col-span-1 col-start-2 text-4xl text-center font-mono antialiased shrink">
+      <div className="w-2/3 flex-row">
+        <h1 className="min-w-1/3 text-4xl text-center font-mono antialiased content-between">
           About
         </h1>
-        <p className="font-mono antialiased text-center row-span-1 row-start-2 col-start-2 shrink">
+        <p className="min-w-1/3 font-mono antialiased text-center content-between">
           I am a passionate developer enthusiastic about learning, solving real problems, and beautiful design.
         </p>
       </div>
